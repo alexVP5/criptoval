b10a2ca507246dbef86c28e66174d9a4124b56ed
AlexVP5 <alex4190550@gmail.com>
2024-10-09 10:25:48 +0300

    получение курса валют

--- criptoval.py
+++ criptoval.py
@@ -1,14 +1,13 @@
 import requests
 import json
-import pprint
 from tkinter import *
 from tkinter import messagebox as mb
+from tkinter import ttk
 
-from Scripts.bottle import response
 
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
     if code:
         try:
             response = requests. get("https://open.er-api.com/v6/latest/USD")
@@ -16,7 +15,7 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate} {code}  за один доллар")
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code}  за один доллар")
             else:
                 mb.showerror("Ошибка!", f"Валюта {code} не найдена!")
         except Exception as e:
@@ -28,9 +27,14 @@ def exchange():
 window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
-Label(text="Введите код валюты").pack(padx=10, pady=10)
-entry = Entry()
-entry.pack(padx=10, pady=10)
+Label(text="Выберете код валюты").pack(padx=10, pady=10)
+
+cur = ['RUB','EUR','GBP','JPY','CNY','KZT','UZS','CHF','AED','CAD']
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
+
+#entry = Entry()
+#entry.pack(padx=10, pady=10)
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
 
 
@@ -38,7 +42,4 @@ window.mainloop()
 
 
 
-'''result = requests. get("https://open.er-api.com/v6/latest/USD")
-data = json.loads(result.text)
-p = pprint.PrettyPrinter(indent=4)'''
 
